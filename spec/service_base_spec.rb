af085969ef15108258b9ff8cad9d461ec8702b8f
owen2345 <owenperedo@gmail.com>
2020-12-21 11:29:32 -0400

    test: fix failed tests

--- spec/service_base_spec.rb
+++ spec/service_base_spec.rb
@@ -7,20 +7,25 @@ RSpec.describe PubSubModelSync::ServiceBase do
 
   describe 'when processing message' do
     before { allow(config).to receive(:log) }
-    it 'does not process if message is coming from same app' do
-      msg = 'Skip message from same origin'
-      payload.headers[:app_key] = 'test_app'
-      allow(config).to receive(:subscription_key).and_return('test_app')
-      expect(config).to receive(:log).with(include(msg), anything)
-      inst.send(:process_message, payload.to_json)
-    end
+    describe 'when checking message source' do
+      before do
+        allow_any_instance_of(described_class).to receive(:same_app_message?).and_call_original
+      end
+      it 'does not process if message is coming from same app' do
+        msg = 'Skip message from same origin'
+        payload.headers[:app_key] = 'test_app'
+        allow(config).to receive(:subscription_key).and_return('test_app')
+        expect(config).to receive(:log).with(include(msg), anything)
+        inst.send(:process_message, payload.to_json)
+      end
 
-    it 'does process if message is coming from different app' do
-      payload.headers[:app_key] = 'unknown_app'
-      allow(config).to receive(:subscription_key).and_return('test_app')
-      expect_any_instance_of(PubSubModelSync::MessageProcessor)
-        .to receive(:process)
-      inst.send(:process_message, payload.to_json)
+      it 'does process if message is coming from different app' do
+        payload.headers[:app_key] = 'unknown_app'
+        allow(config).to receive(:subscription_key).and_return('test_app')
+        expect_any_instance_of(PubSubModelSync::MessageProcessor)
+          .to receive(:process)
+        inst.send(:process_message, payload.to_json)
+      end
     end
   end
 end
