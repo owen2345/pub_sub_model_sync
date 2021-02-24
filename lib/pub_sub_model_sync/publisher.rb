3786c25023a182d517aedac00d118bf7200572b0
owen2345 <owenperedo@gmail.com>
2021-02-24 06:25:16 -0400

    fix: fix payload data mapping

--- lib/pub_sub_model_sync/publisher.rb
+++ lib/pub_sub_model_sync/publisher.rb
@@ -20,14 +20,10 @@ module PubSubModelSync
     private
 
     def payload_data(model)
-      source_props = @attrs.map { |prop| prop.to_s.split(':').first }
-      data = model.as_json(only: source_props, methods: source_props)
-      aliased_props = @attrs.select { |prop| prop.to_s.include?(':') }
-      aliased_props.each do |prop|
+      @attrs.map do |prop|
         source, target = prop.to_s.split(':')
-        data[target] = data.delete(source)
-      end
-      data.symbolize_keys
+        [target || source, model.send(source.to_sym)]
+      end.to_h.symbolize_keys
     end
 
     def payload_attrs(model, action)
@@ -35,15 +31,6 @@ module PubSubModelSync
         klass: (as_klass || model.class.name).to_s,
         action: action.to_sym
       }
-    end
-
-    def payload_headers(model, action)
-      headers = {
-        ordering_key: model.ps_transaction_key(action),
-        key: [model.class.name, action, model.id].join('/')
-      }
-      headers.merge!(model.ps_payload_headers(action))
-      headers
     end
   end
 end
